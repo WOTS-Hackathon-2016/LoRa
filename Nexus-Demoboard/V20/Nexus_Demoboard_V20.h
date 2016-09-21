fa52486dcd1fded8933aa866c8c3348e3bf5a811
Erikscode <erik.van.hilten@ni.com>
2016-09-21 19:08:31 +0200

    fix folders

---
+++ Nexus-Demoboard/V20/Nexus_Demoboard_V20.h
@@ -0,0 +1,64 @@
+/******************************************************************************************
+* Copyright 2015, 2016 Ideetron B.V.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************************/
+/******************************************************************************************
+*
+* File:        Nexus_Demoboard_V20.h
+* Author:      Gerben den Hartog
+* Compagny:    Ideetron B.V.
+* Website:     http://www.ideetron.nl/LoRa
+* E-mail:      info@ideetron.nl
+******************************************************************************************/
+
+/****************************************************************************************
+*
+* Created on: 			19-05-2016
+* Supported Hardware:	ID150170-01 with ID150119-02 with a RFM95
+*
+* History:
+*
+* Firmware version: 1.0
+* First version
+* Firmware version: 2.0
+* New using Encrypt version 3.1
+* Using AppsKey for encryption of data insted of NwkSkey
+****************************************************************************************/
+
+#ifndef NEXUS_DEMOBOARD_V20_H
+#define NEXUS_DEMOBOARD_V20_H
+
+/*
+*****************************************************************************************
+* DEFINES
+*****************************************************************************************
+*/
+
+#define MOV     A0
+#define LED1    A1
+#define LED2    A2
+#define PIR     A3
+#define RES     A6
+#define LDR     A7
+#define DS2401  2
+#define MFP     3
+#define DIO0    4
+#define DIO1    5
+#define DIO5    6
+#define DIO2    7
+#define CS      8
+#define LED_N   9
+
+#endif
